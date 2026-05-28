83ea1d5aaec2dc098483ad5a54e3a519e2864000
QuocccAnhCode <quocanhic20bfudn@gmail.com>
2026-05-28 20:07:31 +0700

    Update Lab1.v

--- Lab1.v
+++ Lab1.v
@@ -2,17 +2,5 @@ module Lab1(
     input  wire [2:0] sw,
     output wire [2:0] led
 );
-    assign led = sw;
+    assign led = ~sw;
 endmodule
-// module Lab1(
-//   input  wire sw1,
-//   input  wire sw2,
-//   input  wire sw3,
-//   output wire led1,
-//   output wire led2,
-//   output wire led3
-// );
-//  assign led1 = sw1;
-//  assign led2 = sw2;
-//  assign led3 = sw3;
-//  endmodule
