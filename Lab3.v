d0d5124d338b9eb8614594f2c753092ee9b1de73
QuocccAnhCode <quocanhic20bfudn@gmail.com>
2026-05-28 14:11:20 +0700

    Update Lab3.v

--- Lab3.v
+++ Lab3.v
@@ -1,26 +1,21 @@
 module Lab3 (
-    input wire clk,       // Tín hiệu xung nhịp hệ thống
-    input wire rst,       // Tín hiệu reset (đưa LED về tắt ban đầu)
-    input wire sw,        // Công tắc
-    output reg led        // Trạng thái đèn LED
+    input wire clk,
+    input wire rst,
+    input wire sw,
+    output reg led
 );
 
-    reg [1:0] sw_sync;    // Thanh ghi để phát hiện cạnh (Edge detection)
-
-    // Khối đồng bộ hóa công tắc và phát hiện cạnh
-    always @(posedge clk or posedge rst) begin
-        if (rst) begin
-            sw_sync <= 2'b00;
+    reg [1:0] sw_sync;
+    // Sửa posedge rst thành negedge rst (bắt sườn xuống khi bấm nút reset)
+    always @(posedge clk or negedge rst) begin
+        if (rst==1'b0) begin // Sửa rst thành !rst (khi nút bị kéo xuống 0)
+            sw_sync <= 2'b11; // Nên khởi tạo sw_sync là 2'b11 vì idle của Active-Low là 1
             led     <= 1'b0;
         end else begin
-            // Dịch bit để lưu trạng thái cũ và mới của công tắc
             sw_sync <= {sw_sync[0], sw};
-
-            // Nếu sw_sync chuyển từ 1 sang 0 (phát hiện cạnh xuống - vừa nhả nút)
-            if (sw_sync == 2'b10) begin
-                led <= ~led; // Đảo trạng thái đèn LED
+            if (sw_sync == 2'b01) begin
+                led <= ~led; 
             end
         end
     end
-
 endmodule
